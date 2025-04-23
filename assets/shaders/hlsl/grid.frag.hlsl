75bd3d511a96f2e7fe315a2728b92deb42022ec1
michael <michaelsnav@gmail.com>
2025-04-23 14:58:15 -0400

    capsule working

--- assets/shaders/hlsl/grid.frag.hlsl
+++ assets/shaders/hlsl/grid.frag.hlsl
@@ -1,9 +1,17 @@
-struct PS_INPUT {
-    float4 Position : SV_POSITION;
-    float4 Color : COLOR;
+struct Input {
+    float4 position : SV_Position;
+    float4 color    : TEXCOORD0;
+    float2 uv       : TEXCOORD1;
 };
 
-float4 main(PS_INPUT input) : SV_TARGET {
-    
-    return input.Color;
+
+float draw_grid(float2 uv) {
+    float2 grid_uv = cos(uv);
+    return max(grid_uv.x, grid_uv.y); 
+}
+
+float4 main(Input input) : SV_Target {
+    float thickness = 20 / 21;
+    float3 color = smoothstep(0.99, 1, draw_grid(input.uv * 20)) * input.color; 
+    return float4(color, 1);
 }
--- assets/shaders/hlsl/grid.vert.hlsl
+++ assets/shaders/hlsl/grid.vert.hlsl
@@ -10,14 +10,15 @@ struct Input {
 struct Output {
     float4 position  : SV_Position;
     float4 color     : TEXCOORD0;
+    float2 uv        : TEXCOORD1;
 };
-
 
 Output main(Input input) {
     Output output;
     
     output.position = mul(mvp, float4(input.position, 1));
     output.color = input.color;
+    output.uv    = max(input.position.xy, input.position.xz);
     
     return output;
 }
